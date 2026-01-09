9b5fe195dd93c090975326d5265ed39fb8563eab
taiki.yoshinaka <taiki.yoshinaka@agc.com>
2026-01-09 04:29:10 +0000

    Remove proxy import and hardcoded path from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from 'next';
 
-// Import proxy setup
-import './next.config.proxy.js';
-
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: '/home/yoshinaka/devel/Asset-Integration',
   experimental: {
     serverActions: {
       bodySizeLimit: '10mb',
